26fd4dccee5eda9b85cfee69faaf42e5ffc5c5ca
parisa <parisa.abbas@gmail.com>
2026-05-03 21:32:29 +0800

    readme draft

--- SimpleMultiplication.java
+++ SimpleMultiplication.java
@@ -23,7 +23,7 @@ public class SimpleMultiplication {
         String finalAns = multi(firstNumString, secondNumString);
         System.out.println();
         System.out.println("The length of the answer is " + finalAns.length());
-        //System.out.println("The product of the two terms is " + finalAns);
+        // System.out.println("The product of the two terms is " + finalAns);
 
     }
 
